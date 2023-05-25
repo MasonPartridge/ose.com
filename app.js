4a76a394d3ba592e684abe0326684dcec29ac800
MasonPartridge <masonepartridge@gmail.com>
2023-05-25 16:52:22 -0700

    Integrate ose-database route into website

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var oseDatabaseRouter = require('./routes/ose-database');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+app.use('/ose-database', oseDatabaseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
