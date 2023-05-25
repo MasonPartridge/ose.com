2f973af1377c0c80d21c68fc16ba97789fa67399
MasonPartridge <masonepartridge@gmail.com>
2023-05-25 15:52:23 -0700

    Rename author model to personal

--- models/personal.js
+++ models/personal.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const AuthorSchema = new Schema({
+const PersonalSchema = new Schema({
     first_name: { type: String, required: true },
     family_name: { type: String, required: true },
     date_of_birth: { type: Date },
@@ -11,7 +11,7 @@ const AuthorSchema = new Schema({
 });
 
 AuthorSchema.virtual("url").get(function () {
-    return `/catalog/author/${this.id}`;
+    return `/catalog/personal/${this.id}`;
 });
 
-module.exports = mongoose.model("Author", AuthorSchema);
+module.exports = mongoose.model("Personal", AuthorSchema);
