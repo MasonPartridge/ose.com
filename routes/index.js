1a4d0521e7756e400f1c5120aca933d1440fdf47
MasonPartridge <masonepartridge@gmail.com>
2023-05-26 07:01:57 -0700

    Create home page boilerplate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.redirect("/ose-database");
+  res.render('index', {title: 'OSE Corporation'});
 });
 
 module.exports = router;
