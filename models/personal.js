ba9f8c669d645bd7e9086e7ce0e2b4d528e1a4fd
MasonPartridge <masonepartridge@gmail.com>
2023-05-28 17:46:40 -0700

    Remove the requirement for the family name on personal model

--- models/personal.js
+++ models/personal.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const PersonalSchema = new Schema({
     first_name: { type: String, required: true },
-    family_name: { type: String, required: true },
+    family_name: { type: String },
     date_of_birth: { type: Date },
     date_of_death: { type: Date },
     clearance_level: { type: String, enum: [ "I", "II", "III", "IV", "V"] }
