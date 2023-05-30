236381e1db4064772efeb46d46e77c78456d9e70
MasonPartridge <masonepartridge@gmail.com>
2023-05-29 17:09:11 -0700

    Add Personal addition form to personal.js router

--- routes/personal.js
+++ routes/personal.js
@@ -8,4 +8,6 @@ router.get("/", personal_controller.personal_list);
 
 router.get("/:id", personal_controller.personal_info);
 
+router.get("/employee-form", personal_controller.employee_form_get);
+
 module.exports = router;
