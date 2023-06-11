1de4820445c08793d3471464c60248fa4c2ebe9e
MasonPartridge <masonepartridge@gmail.com>
2023-06-11 00:59:51 -0700

    Rework page templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {title: 'OSE Corporation'});
+  res.render('index', {title: 'OSE Corporation', inputPage: './mainpage.ejs'});
 });
 
 module.exports = router;
