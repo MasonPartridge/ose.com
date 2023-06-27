09ea8e257fdebfe1a9927259bd591053edbbd7bf
MasonPartridge <masonepartridge@gmail.com>
2023-06-27 08:47:02 -0700

    Add employee-delete to router

--- routes/employee-database.js
+++ routes/employee-database.js
@@ -10,6 +10,8 @@ router.get("/employee-form", personal_controller.employee_form_get);
 
 router.get("/edit-form/:id", personal_controller.employee_edit);
 
+router.get("employee-delete", personal_controller.employee_delete);
+
 router.get("/:id", personal_controller.employee_info);
 
 router.post("/employee-form", personal_controller.employee_form_post);
